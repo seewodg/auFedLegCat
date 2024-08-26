e53e75f1e6ce87f3b82a8eaf72e98eebf8b6a3e9
seewodg <seewodg@gmail.com>
2024-08-26 22:52:24 +1000

    Add ability to search legislation.gov.au
    Added script and refactored existing script to enable scraping search returns at legislation.gov.au and generate both dcat:Dataset instances and a dcat:Catalog that indexes the dcat:Dataset instances.

--- auFedLegCat/pysource/config.py
+++ auFedLegCat/pysource/config.py
@@ -11,9 +11,13 @@ CONFIG_INFO = {
     # C2004A04997
     # C1901A00006
     # C2004A05138
-    "legID": "C2004A05138",
+    # C2004A04121
+    # C2004A01224
+    # C2020A00012
+    "legID": "C2020A00012",
     "tableOfContents": True,
     "pageMetadata": True,
     "detailedMetadata": True,
-    "outputFolder": "./output/"
+    "outputFolder": "./output/",
+    "catalogTarget": "https://www.legislation.gov.au/search/text(%22carbon%22,nameAndText,any)/status(InForce)/pointintime(Latest)/type(Principal)/collection(Act,LegislativeInstrument)/administeringdepartments(%22O-007311%22)/sort(searchcontexts%252Ffulltextversion%252Fregisteredat%2520desc)"
 }
---
+++ auFedLegCat/pysource/mineLegIdentifiers.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 26 13:37:46 2024
+
+@author: marcu
+"""
+from rdflib import Graph, URIRef, XSD, DCAT, SKOS, DCTERMS, RDF, RDFS, Literal, OWL
+from bs4 import BeautifulSoup
+from bs4.dammit import EncodingDetector
+import requests
+import datetime
+from time import sleep
+from config import CONFIG_INFO
+import urllib.parse
+import genericDatasetExample as gendataset
+
+def init():
+    # get the target URL - e.g. search URL from https://legislation.gov.au
+    target = CONFIG_INFO["catalogTarget"]
+    global source_url; source_url = urllib.parse.unquote(target)
+    g = Graph()
+    # scrape the search results to build the catalog and datasets
+    scrapeSearchResults(g, source_url)
+
+def scrapeSearchResults(g, source_url):
+    parser = 'html.parser'  # or 'lxml' (preferred) or 'html5lib', if installed
+    resp = requests.get(source_url)
+    http_encoding = resp.encoding if 'charset' in resp.headers.get('content-type', '').lower() else None
+    html_encoding = EncodingDetector.find_declared_encoding(resp.content, is_html=True)
+    encoding = html_encoding or http_encoding
+    soup = BeautifulSoup(resp.content, parser, from_encoding=encoding)
+    
+    # begin building catalog graph - first the prefixes
+    baseURL = "http://example.org/au/leg/catalog/"
+    namesp = URIRef(baseURL)
+    g.bind("legcat", namesp)
+    skosURL = "http://example.org/au/leg/concepts/"
+    g.bind("legcons", skosURL)
+    g.bind('rdf', RDF)
+    g.bind('rdfs', RDFS)
+    g.bind('skos', SKOS)
+    g.bind('dcat', DCAT)
+    g.bind('dct', DCTERMS)
+    g.bind('xsd', XSD)
+    g.bind('owl', OWL)
+    g.bind('vcard', URIRef("http://www.w3.org/2006/vcard/ns#"))
+    g.bind('ocmv', URIRef("https://w3id.org/ontouml-models/vocabulary#"))
+    # build the catalog minus the dcat:Datasets
+    g.add((namesp, RDF.type, OWL.Class))
+    g.add((namesp, RDF.type, DCAT.Resource))
+    g.add((namesp, RDF.type, DCAT.Catalog))
+    g.add((namesp, DCTERMS.title, Literal("Australian Legislation Catalog Example", lang="en")))
+    g.add((namesp, RDFS.label, Literal("Australian Legislation Catalog Example", lang="en")))
+    g.add((namesp, DCTERMS.alternative, Literal("UNTP/AuLeg Catalog", lang="en")))
+    g.add((namesp, DCTERMS.description, Literal("""The Australian Legislation Catalog Example, short-named UNTP/AuLeg Catalog, is a structured and open-source catalog that refers to RDF datasets generated from Australian Legislation.
+
+It was designed to provide an example for research to support UNTP Digital Product Passports, but could be applied in other applications.""", lang="en")))
+    g.add((namesp, URIRef("https://w3id.org/ontouml-models/vocabulary#storageUrl"), URIRef("https://github.com/seewodg/auFedLegCat/tree/main/auFedLegCat/catalog/")))
+    g.add((namesp, DCAT.themeTaxonomy, URIRef("http://example.org/au/leg/concepts/")))
+    g.add((namesp, DCTERMS.bibliographicCitation, Literal("Marcus Jowsey et al. (UNECE Team 2024).", lang="en")))
+    g.add((namesp, DCTERMS.license, URIRef("https://creativecommons.org/licenses/by-sa/4.0/")))
+    g.add((namesp, DCTERMS.issued, Literal("2024-06-16T17:53:16.365007", datatype=XSD['dateTime'])))
+    g.add((namesp, DCTERMS.modified, Literal(datetime.datetime.now(), datatype=XSD['dateTime'])))
+    g.add((namesp, DCTERMS.publisher, URIRef("https://orcid.org/0009-0007-8434-7325")))
+    g.add((namesp, DCTERMS.creator, URIRef("https://orcid.org/0009-0007-8434-7325")))
+    # itterate through the legislation IDs returned in a scrape and add them as dcat:Dataset references in the catalog
+    cnt = 1
+    if addDatasetReferences(g, soup, namesp, cnt) is True:
+        print("Successfully cataloged datasets")
+    else: print("No datasets cataloged")
+    
+def addDatasetReferences(g, soup, namesp, cnt):
+    for span in soup.find_all('span', {'class': 'title-id'}, recursive=True):
+        if span is not None:
+            legid = span.string
+            addDataset(legid)
+            resource = f"http://example.org/au/leg/dataset/{legid}/"
+            g.add((namesp, DCAT.dataset, URIRef(resource))) # add the dcat:Dataset reference to the catalog
+            # print(f"Span: {legid} Count: {cnt}")
+            cnt += 1
+            sleep(10) # waite 10 seconds out of respect for robots.txt
+    g.serialize('./output/lawCatalog.ttl', format='ttl') # generate the dcat:Dataset
+    if (namesp, DCAT.dataset, None) in g:
+        return True
+    else: return False
+    
+def addDataset(legid):
+    result = gendataset.init(legid) # create the dataset
+    return result
+
+if __name__ == "__main__":
+    init()
