1a36eb88eb4add0efe44b9d5e03e6e57a9698850
seewodg <seewodg@gmail.com>
2026-05-28 11:14:22 +1000

    Changes to scripts and data

--- auFedLegCat/pysource/config.py
+++ auFedLegCat/pysource/config.py
@@ -18,6 +18,18 @@ CONFIG_INFO = {
     "tableOfContents": True,
     "pageMetadata": True,
     "detailedMetadata": True,
+<<<<<<< Updated upstream
     "outputFolder": "./output/",
     "catalogTarget": "https://www.legislation.gov.au/search/text(%22carbon%22,nameAndText,any)/status(InForce)/pointintime(Latest)/type(Principal)/collection(Act,LegislativeInstrument)/administeringdepartments(%22O-007311%22)/sort(searchcontexts%252Ffulltextversion%252Fregisteredat%2520desc)"
+=======
+    "pagelimit": 1,
+    "outputFolder": "../output/",
+    "catalogName": "discloseLawCatalog",
+#    "catalogTarget": "https://www.legislation.gov.au/search/text(%22marine%22,nameAndText,contains)/status(InForce)/pointintime(Latest)/collection(Act,AdministrativeArrangementsOrder)/sort(searchcontexts%2Ftext%2Frelevance%20desc))"
+#    "catalogTarget": "https://www.legislation.gov.au/search/text(%22agriculture%22,nameAndText,contains)/status(InForce)/pointintime(Latest)/collection(Act,AdministrativeArrangementsOrder)/sort(searchcontexts%2Ftext%2Frelevance%20desc)"
+#    "catalogTarget": "https://www.legislation.gov.au/search/text(%22location%20spatial%20area%22,nameAndText,all)/status(InForce)/pointintime(Latest)/collection(Act,AdministrativeArrangementsOrder,LegislativeInstrument)/sort(searchcontexts%2Ftext%2Frelevance%20desc)"
+#    "catalogTarget": "https://www.legislation.gov.au/search/text(%22information%20technology%22,nameAndText,contains)/status(InForce)/pointintime(Latest)/collection(Act,LegislativeInstrument)/sort(searchcontexts%2Ftext%2Frelevance%20desc)"
+#    "catalogTarget": "https://www.legislation.gov.au/search/text(%22non%20disclosure%20agreement%22,nameAndText,all)/status(InForce)/pointintime(Latest)/collection(Act,LegislativeInstrument)/sort(searchcontexts%2Ftext%2Frelevance%20desc)"
+     "catalogTarget": "https://www.legislation.gov.au/search/text(%22disclosure%20shareholder%22,nameAndText,all)/status(InForce)/pointintime(Latest)/collection(Act,LegislativeInstrument)/sort(searchcontexts%2Ftext%2Frelevance%20desc)"
+>>>>>>> Stashed changes
 }
