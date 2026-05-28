08d9285443206f7ceb49a45ddb963134e208369b
Marcus Jowsey <42772947+seewodg@users.noreply.github.com>
2026-05-28 11:24:49 +1000

    Refactor config.py by cleaning legID comments
    Removed commented legIDs and updated output folder path.

--- auFedLegCat/pysource/config.py
+++ auFedLegCat/pysource/config.py
@@ -6,22 +6,9 @@ Created on Wed Aug 14 16:43:38 2024
 """
 
 CONFIG_INFO = {
-    # C1901A00002
-    # C2022A00037
-    # C2004A04997
-    # C1901A00006
-    # C2004A05138
-    # C2004A04121
-    # C2004A01224
-    # C2020A00012
-    "legID": "C2020A00012",
     "tableOfContents": True,
     "pageMetadata": True,
     "detailedMetadata": True,
-<<<<<<< Updated upstream
-    "outputFolder": "./output/",
-    "catalogTarget": "https://www.legislation.gov.au/search/text(%22carbon%22,nameAndText,any)/status(InForce)/pointintime(Latest)/type(Principal)/collection(Act,LegislativeInstrument)/administeringdepartments(%22O-007311%22)/sort(searchcontexts%252Ffulltextversion%252Fregisteredat%2520desc)"
-=======
     "pagelimit": 1,
     "outputFolder": "../output/",
     "catalogName": "discloseLawCatalog",
@@ -31,5 +18,4 @@ CONFIG_INFO = {
 #    "catalogTarget": "https://www.legislation.gov.au/search/text(%22information%20technology%22,nameAndText,contains)/status(InForce)/pointintime(Latest)/collection(Act,LegislativeInstrument)/sort(searchcontexts%2Ftext%2Frelevance%20desc)"
 #    "catalogTarget": "https://www.legislation.gov.au/search/text(%22non%20disclosure%20agreement%22,nameAndText,all)/status(InForce)/pointintime(Latest)/collection(Act,LegislativeInstrument)/sort(searchcontexts%2Ftext%2Frelevance%20desc)"
      "catalogTarget": "https://www.legislation.gov.au/search/text(%22disclosure%20shareholder%22,nameAndText,all)/status(InForce)/pointintime(Latest)/collection(Act,LegislativeInstrument)/sort(searchcontexts%2Ftext%2Frelevance%20desc)"
->>>>>>> Stashed changes
 }
