df517fcf39345f327c0dfc0404101e7e93c83990
seewodg <seewodg@gmail.com>
2026-05-28 11:25:58 +1000

    fixed config on remote

--- auFedLegCat/pysource/config.py
+++ auFedLegCat/pysource/config.py
@@ -6,18 +6,10 @@ Created on Wed Aug 14 16:43:38 2024
 """
 
 CONFIG_INFO = {
-    # C1901A00002
-    # C2022A00037
-    # C2004A04997
-    # C1901A00006
-    # C2004A05138
-    # C2004A04121
-    # C2004A01224
-    # C2020A00012
-    "legID": "C2020A00012",
     "tableOfContents": True,
     "pageMetadata": True,
     "detailedMetadata": True,
+<<<<<<< HEAD
 <<<<<<< Updated upstream
     "outputFolder": "./output/",
     "catalogTarget": "https://www.legislation.gov.au/search/text(%22carbon%22,nameAndText,any)/status(InForce)/pointintime(Latest)/type(Principal)/collection(Act,LegislativeInstrument)/administeringdepartments(%22O-007311%22)/sort(searchcontexts%252Ffulltextversion%252Fregisteredat%2520desc)"
@@ -32,4 +24,11 @@ CONFIG_INFO = {
 #    "catalogTarget": "https://www.legislation.gov.au/search/text(%22non%20disclosure%20agreement%22,nameAndText,all)/status(InForce)/pointintime(Latest)/collection(Act,LegislativeInstrument)/sort(searchcontexts%2Ftext%2Frelevance%20desc)"
      "catalogTarget": "https://www.legislation.gov.au/search/text(%22disclosure%20shareholder%22,nameAndText,all)/status(InForce)/pointintime(Latest)/collection(Act,LegislativeInstrument)/sort(searchcontexts%2Ftext%2Frelevance%20desc)"
 >>>>>>> Stashed changes
+=======
+    "pagelimit": 2,
+    "outputFolder": "../output/",
+    "catalogName": "agricultureLawCatalog",
+#    "catalogTarget": "https://www.legislation.gov.au/search/text(%22marine%22,nameAndText,contains)/status(InForce)/pointintime(Latest)/collection(Act,AdministrativeArrangementsOrder)/sort(searchcontexts%2Ftext%2Frelevance%20desc))"
+    "catalogTarget": "https://www.legislation.gov.au/search/text(%22agriculture%22,nameAndText,contains)/status(InForce)/pointintime(Latest)/collection(Act,AdministrativeArrangementsOrder)/sort(searchcontexts%2Ftext%2Frelevance%20desc)"
+>>>>>>> b4e7bce1a071e4d219e35b0886abdc290b3c940f
 }
